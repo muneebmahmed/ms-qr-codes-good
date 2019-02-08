e1f671c9ab20e66c414325e9e1e03f6ecc74c41c
Amanda Chesin <achesin@email.arizona.edu>
2019-02-07 17:00:56 -0700

    Fitting Page to the Models

--- app/transactionHistory.js
+++ app/transactionHistory.js
@@ -13,24 +13,31 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.headline}>Transaction History </Text>
-        <Button
+        <View style={{flexDirection: 'row', textAlign: 'center', justifyContent: 'center'}}>
+          <Button
             onPress={() => this.props.action('Home')}
             title="Received"
             type="outline"
             accessibilityLabel="See Received Transactions"
-        />
-        <Button
+          />
+          <Button
             onPress={() => this.props.action('Home')}
             title="Paid"
             type="outline"
             accessibilityLabel="See Paid Transactions"
-        />
+          />
+        </View>
         <ScrollView>
-          <Text style={{fontSize:26}}>$10.00</Text>
-          <Text style={{fontSize:16}}>1/22/2019</Text>
-          <View style={styles.row}>
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}}             />
-            <Text style={{fontSize:16, textAlign: "right"}}>You paid John</Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize:26}}>-</Text>
+            <Text style={{fontSize:26}}>$10.00</Text>
+          </View>
+          <View style={styles.leftContainer}>
+            <Text style={{fontSize:16}}>1/22/2019</Text>
+            <View style={styles.rightContainer}>
+              <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}}             />
+              <Text style={{fontSize:16, textAlign: 'right'}}>You paid John</Text>
+            </View>
           </View>
           <View
             style={{
@@ -38,40 +45,85 @@ export default class App extends React.Component {
               borderBottomWidth: 1,
             }}
           />
-          <Text style={{fontSize:26}}>$15.00</Text>
-          <Text style={{fontSize:16}}>12/18/2018</Text>
-          <Text style={{fontSize:16, textAlign: "right"}}>You paid Kim</Text>
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}} />
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize:26}}>-</Text>
+            <Text style={{fontSize:26}}>$15.00</Text>
+          </View>
+          <View style={styles.leftContainer}>
+            <Text style={{fontSize:16}}>12/18/2018</Text>
+            <View style={styles.rightContainer}>
+              <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}}             />
+              <Text style={{fontSize:16, textAlign: 'right'}}>You paid Kim</Text>
+            </View>
+          </View>
           <View
             style={{
               borderBottomColor: 'black',
               borderBottomWidth: 1,
             }}
           />
-          <Text style={{fontSize:26}}>$3.00</Text>
-          <Text style={{fontSize:16}}>10/13/2018</Text>
-          <Text style={{fontSize:16, textAlign: "right"}}>You paid Sally</Text>
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}} />
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize:26}}>-</Text>
+            <Text style={{fontSize:26}}>$3.00</Text>
+          </View>
+          <View style={styles.leftContainer}>
+            <Text style={{fontSize:16}}>10/13/2018</Text>
+            <View style={styles.rightContainer}>
+              <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}}             />
+              <Text style={{fontSize:16, textAlign: 'right'}}>You paid Sally</Text>
+            </View>
+          </View>
           <View
             style={{
               borderBottomColor: 'black',
               borderBottomWidth: 1,
             }}
           />
-          <Text style={{fontSize:26}}>$5.00</Text>
-          <Text style={{fontSize:16}}>10/07/2018</Text>
-          <Text style={{fontSize:16, textAlign: "right"}}>You paid Lara</Text>
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}} />
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize:26}}>-</Text>
+            <Text style={{fontSize:26}}>$5.00</Text>
+          </View>
+          <View style={styles.leftContainer}>
+            <Text style={{fontSize:16}}>10/07/2018</Text>
+            <View style={styles.rightContainer}>
+              <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}}             />
+              <Text style={{fontSize:16, textAlign: 'right'}}>You paid Lara</Text>
+            </View>
+          </View>
         <View
             style={{
               borderBottomColor: 'black',
               borderBottomWidth: 1,
             }}
           />
-          <Text style={{fontSize:26}}>$1.50</Text>
-          <Text style={{fontSize:16}}>9/30/2018</Text>
-          <Text style={{fontSize:16, textAlign: "right"}}>You paid Jack</Text>
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}} />
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize:26}}>-</Text>
+            <Text style={{fontSize:26}}>$1.50</Text>
+          </View>
+          <View style={styles.leftContainer}>
+            <Text style={{fontSize:16}}>9/30/2018</Text>
+            <View style={styles.rightContainer}>
+              <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}}             />
+              <Text style={{fontSize:16, textAlign: 'right'}}>You paid Jack</Text>
+            </View>
+          </View>
+          <View
+            style={{
+              borderBottomColor: 'black',
+              borderBottomWidth: 1,
+            }}
+          />
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize:26}}>-</Text>
+            <Text style={{fontSize:26}}>$3.00</Text>
+          </View>
+          <View style={styles.leftContainer}>
+            <Text style={{fontSize:16}}>10/13/2018</Text>
+            <View style={styles.rightContainer}>
+              <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 34, height: 34}}             />
+              <Text style={{fontSize:16, textAlign: 'right'}}>You paid Sally</Text>
+            </View>
+          </View>
         </ScrollView>
       </View>
       );
@@ -89,12 +141,28 @@ const styles = StyleSheet.create({
    headline: {
     textAlign: 'center',
     fontWeight: 'bold',
-    fontSize: 18,
+    fontSize: 36,
     marginTop: 0,
     width: '100%',
+    padding: 10
   },
   row: {
     flexDirection: "row",
-    textAlign: 'right'
+    textAlign: 'right',
+    padding: 10,
+    justifyContent: 'flex-end'
+  },
+   leftContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    padding: 10
+  },
+  rightContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    padding: 5
   }
 });
