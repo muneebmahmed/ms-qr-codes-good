6ff7f55a48143b969051b72e834768e4fb7b4056
applepieprogramming <applepieprogramming@gmail.com>
2019-02-11 22:45:54 -0700

    added store object and gave example on how to manipulate store. And how store persists accross all pages

--- app/settings.js
+++ app/settings.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Button, Text, View } from 'react-native';
 import {styles} from './styles'
+import {store} from './store'
 
 export default class Settings extends React.Component {
+  changeName (Aname) {
+   store.name = Aname;
+  }
   render() {
     const {navigate} = this.props.navigation;
     return (
@@ -11,6 +15,12 @@ export default class Settings extends React.Component {
         <Button
             onPress={() => navigate('Home')}
             title="go home"
+        />
+        <Text>The button bellow will change the name!</Text>
+        <Text>Then go home to see what it has changed to</Text>
+        <Button
+            onPress={() => this.changeName('Corey')}
+            title="Change Name"
         />
       </View>
       ); 
