45307025928affd4dba84808bce519e927ceb5a9
Amanda Chesin <achesin@email.arizona.edu>
2019-02-12 17:46:11 -0700

    created credit card element

---
+++ app/creditcard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button, Text, View } from 'react-native';
+import {styles} from './styles'
+
+export default class CreditCard extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+      );
+
+  }
+}
