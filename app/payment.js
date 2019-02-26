ceca6c824b37c6df525f045fb98f31b2d52cdfe1
Amanda Chesin <achesin@email.arizona.edu>
2019-02-26 14:36:31 -0700

    Start on Payments

--- app/payment.js
+++ app/payment.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import {Button, Text, View } from 'react-native';
+import {Button, Text, View, TextInput } from 'react-native';
 import {styles} from './styles'
 
 export default class Payments extends React.Component {
   render() {
-    const {navigate} = this.props.navigation;
+    constructor(props) {
+      super(props);
+      this.state = { text: '$10.00' };
+    }
     return (
-      <View style={styles.container}>
-        <Text>This is the Payments Page</Text>
-        <Button
-            onPress={() => navigate('Home')}
-            title="go home"
+      <View>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1, justifyContent: 'center'}}
+          keyboardType='numeric'
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
         />
+        <Button
+          onPress={() => this.props.increase}
+          title="Plus One"/>
+        <Button
+          onPress={() => this.props.decrease}
+          title="Minus One"/>
       </View>
-      ); 
+      );
 
   }
 }
