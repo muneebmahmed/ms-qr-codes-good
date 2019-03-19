daaa25dc63ca5604ba31783a78d55bb9d3696d28
Amanda Chesin <achesin@email.arizona.edu>
2019-03-19 15:34:12 -0700

    should look right

--- app/myQR.js
+++ app/myQR.js
@@ -14,20 +14,17 @@ export default class myQR extends React.Component {
               borderBottomWidth: 1,
             }}
           />
-          <View style={{flexDirection: 'row'}}>
-            <Text style={{fontSize:26}}>-</Text>
-            <Text style={{fontSize:26}}>$10.00</Text>
-          </View>
-          <View style={styles.leftContainer}>
-            <Text style={{fontSize:16}}>1/22/2019</Text>
-            <View style={styles.rightContainer}>
-              <Image
-              style={styles.keepitsmall}
-              source={require('./images/user.png')} />
-              <Text style={{fontSize:16, textAlign: 'right'}}>You paid</Text>
-              <Text style={{fontSize:16, textAlign: 'right'}}> John</Text>
-            </View>
-          </View>
+          <View style={styles.container}>
+              <Text style={{fontSize:26}}>
+                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 70, height: 70}}             />
+              Donation
+              </Text>
+              <View style={{flexDirection: 'row'}}>
+              <Text style={{fontSize:20}}>                Default: </Text>
+              <Text style={{fontSize:20}}>$10.00</Text>
+              </View>
+              <Text style={{fontSize:20}}>                Me</Text>
+              </View>
           <View
             style={{
               borderBottomColor: 'black',
@@ -35,98 +32,16 @@ export default class myQR extends React.Component {
             }}
           />
           <View style={{flexDirection: 'row'}}>
-            <Text style={{fontSize:26}}>-</Text>
-            <Text style={{fontSize:26}}>$15.00</Text>
-          </View>
-          <View style={styles.leftContainer}>
-            <Text style={{fontSize:16}}>12/18/2018</Text>
-            <View style={styles.rightContainer}>
-            <Image
-            style={styles.keepitsmall}
-            source={require('./images/anonymoususer.png')} />
-              <Text style={{fontSize:16, textAlign: 'right'}}>You paid </Text>
-              <Text style={{fontSize:16, textAlign: 'right'}}> Kim</Text>
+            <View style={styles.container}>
+            <Text style={{fontSize:26}}>
+              <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 70, height: 70}}             />
+              Donation
+            </Text>
+            <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize:20}}>                Default: </Text>
+            <Text style={{fontSize:20}}>$10.00</Text>
             </View>
-          </View>
-          <View
-            style={{
-              borderBottomColor: 'black',
-              borderBottomWidth: 1,
-            }}
-          />
-          <View style={{flexDirection: 'row'}}>
-            <Text style={{fontSize:26}}>-</Text>
-            <Text style={{fontSize:26}}>$3.00</Text>
-          </View>
-          <View style={styles.leftContainer}>
-            <Text style={{fontSize:16}}>10/13/2018</Text>
-            <View style={styles.rightContainer}>
-            <Image
-            style={styles.keepitsmall}
-            source={require('./images/user.png')} />
-              <Text style={{fontSize:16, textAlign: 'right'}}>You paid</Text>
-              <Text style={{fontSize:16, textAlign: 'right'}}> Sally</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              borderBottomColor: 'black',
-              borderBottomWidth: 1,
-            }}
-          />
-          <View style={{flexDirection: 'row'}}>
-            <Text style={{fontSize:26}}>-</Text>
-            <Text style={{fontSize:26}}>$5.00</Text>
-          </View>
-          <View style={styles.leftContainer}>
-            <Text style={{fontSize:16}}>10/07/2018</Text>
-            <View style={styles.rightContainer}>
-            <Image
-            style={styles.keepitsmall}
-            source={require('./images/anonymoususer.png')} />
-              <Text style={{fontSize:16, textAlign: 'right'}}>You paid</Text>
-              <Text style={{fontSize:16, textAlign: 'right'}}> Lara</Text>
-            </View>
-          </View>
-        <View
-            style={{
-              borderBottomColor: 'black',
-              borderBottomWidth: 1,
-            }}
-          />
-          <View style={{flexDirection: 'row'}}>
-            <Text style={{fontSize:26}}>-</Text>
-            <Text style={{fontSize:26}}>$1.50</Text>
-          </View>
-          <View style={styles.leftContainer}>
-            <Text style={{fontSize:16}}>9/30/2018</Text>
-            <View style={styles.rightContainer}>
-            <Image
-            style={styles.keepitsmall}
-            source={require('./images/user.png')} />
-              <Text style={{fontSize:16, textAlign: 'right'}}>You paid</Text>
-              <Text style={{fontSize:16, textAlign: 'right'}}> Jack</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              borderBottomColor: 'black',
-              borderBottomWidth: 1,
-            }}
-          />
-          <View style={{flexDirection: 'row'}}>
-            <Text style={{fontSize:26}}>-</Text>
-            <Text style={{fontSize:26}}>$7.00</Text>
-          </View>
-          <View style={styles.leftContainer}>
-            <Text style={{fontSize:16}}>10/13/2018</Text>
-            <View style={styles.rightContainer}>
-            <Image
-            style={styles.keepitsmall}
-            source={require('./images/user.png')} />
-              <Text style={{fontSize:16, textAlign: 'right'}}>You paid </Text>
-              <Text style={{fontSize:16, textAlign: 'right'}}> Rachel</Text>
-            </View>
+            <Text style={{fontSize:20}}>                Me</Text>
           </View>
         </ScrollView>
       </View>
